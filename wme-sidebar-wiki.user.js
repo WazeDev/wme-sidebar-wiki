c3516fbfd2bf3922370d19f72702e36c5784447a
Gavin Canon-Phratsachack <37220586+gncnpk@users.noreply.github.com>
2025-06-25 17:30:10 -0500

    Update wme-sidebar-wiki.user.js

--- wme-sidebar-wiki.user.js
+++ wme-sidebar-wiki.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Sidebar Wiki
 // @namespace    https://github.com/WazeDev/wme-sidebar-wiki
-// @version      0.0.1
+// @version      0.0.2
 // @description  Adds the wiki to the sidebar.
 // @author       Gavin Canon-Phratsachack (https://github.com/gncnpk)
 // @match        https://beta.waze.com/*editor*
